cce9d69b0a2c699a62dd14d05998e9982051acb7
Talitha Voortman <t.g.i.voortman@st.hanze.nl>
2019-10-31 14:46:22 +0100

    Requirement 8, verplaatsen van de queenbee

--- src/main/java/nl/hanze/hive/QueenBee.java
+++ src/main/java/nl/hanze/hive/QueenBee.java
@@ -2,13 +2,13 @@ package nl.hanze.hive;
 
 import java.util.ArrayList;
 
-public class Beetle {
-
+public class QueenBee {
     private ArrayList<ArrayList<Integer>> getPossibleMoves(int fromQ, int fromR, Board board) {
         ArrayList<ArrayList<Integer>> possibleMoves = new ArrayList<>();
         ArrayList<ArrayList<Integer>> maybeMoves = board.getSurroundingTiles(fromQ, fromR);
         for (ArrayList<Integer> maybeMove : maybeMoves) {
-            if (board.canStoneSlide(fromQ, fromR, maybeMove.get(0), maybeMove.get(1))) {
+            if (board.canStoneSlide(fromQ, fromR, maybeMove.get(0), maybeMove.get(1)) &&
+                    board.getHeightStoneStackOnSpot(maybeMove.get(0), maybeMove.get(1)) == 0) {
                 possibleMoves.add(maybeMove);
             }
         }
@@ -25,3 +25,4 @@ public class Beetle {
         return false;
     }
 }
+
---
+++ src/test/java/nl/hanze/hive/QueenBeeSpec.java
@@ -0,0 +1,43 @@
+package nl.hanze.hive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QueenBeeSpec {
+    @Test
+    void givenBeetleCanSlideOneTileThenTrue() {
+        Board board = new Board();
+        QueenBee queenBee = new QueenBee();
+        int fromQ = 0;
+        int fromR = 0;
+        int toQ = 1;
+        int toR = 0;
+        Stone stoneQueenBee = new Stone(Hive.Player.WHITE, Hive.Tile.QUEEN_BEE);
+        Stone stoneBeetle = new Stone(Hive.Player.WHITE, Hive.Tile.BEETLE);
+        board.setTile(fromQ, fromR, stoneQueenBee);
+        board.setTile(1,-1, stoneBeetle);
+        boolean canQueenBeeMoveOneTile = queenBee.isMoveAllowed(fromQ, fromR, toQ, toR, board);
+        toQ = 2;
+        assertTrue(canQueenBeeMoveOneTile);
+        boolean canQueenBeeMoveTwoTiles = queenBee.isMoveAllowed(fromQ, fromR, toQ, toR, board);
+        assertFalse(canQueenBeeMoveTwoTiles);
+    }
+
+    @Test
+    void givenQueenBeeMovesOnTopOfStoneThenFalse() {
+        Board board = new Board();
+        QueenBee queenBee = new QueenBee();
+        int fromQ = 0;
+        int fromR = 0;
+        int toQ = 1;
+        int toR = 0;
+        Stone stoneQueenBee = new Stone(Hive.Player.WHITE, Hive.Tile.QUEEN_BEE);
+        Stone stoneBeetle = new Stone(Hive.Player.WHITE, Hive.Tile.BEETLE);
+        board.setTile(fromQ, fromR, stoneQueenBee);
+        board.setTile(toQ, toR, stoneBeetle);
+        boolean canQueenBeeMoveOnTopOfTile = queenBee.isMoveAllowed(fromQ, fromR, toQ, toR, board);
+        assertFalse(canQueenBeeMoveOnTopOfTile);
+    }
+}
